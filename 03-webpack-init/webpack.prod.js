cfdfdf6de45083598b8223fc0c4219d59a689730
anbreaker <antunez19@gmail.com>
2021-07-14 10:56:34 +0200

    feat: install babel and config production

--- 03-webpack-init/webpack.prod.js
+++ 03-webpack-init/webpack.prod.js
@@ -49,6 +49,17 @@ module.exports = {
         ],
         type: 'javascript/auto',
       },
+
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
 
